a98df0767e95f1b337eb45bb1dcb5fecf7a93b10
Luan Antony <luanantony29@gmail.com>
2023-11-09 20:30:46 -0300

    Update App.js
    Atualização do App.js

--- App.js
+++ App.js
@@ -1,23 +1,42 @@
+// Exercicio 02
+
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import MaxMin from './components/MaxMin';
+import { View } from 'react-native';
+import Contador from './components/Contador';
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <MaxMin x={23} y={0} /> 
-      <MaxMin x={23} y={18} /> 
+    <View>
+      <Contador />
     </View>
   );
 }
 
 export default App;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'gray',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-});
+
+// Exercicio 01
+
+// import React from 'react';
+// import { View, StyleSheet } from 'react-native';
+// import MaxMin from './components/MaxMin';
+
+// const App = () => {
+//   return (
+//     <View style={styles.container}>
+//       <MaxMin x={10} y={0} /> 
+//       <MaxMin x={10} y={10} /> 
+//     </View>
+//   );
+// }
+
+// export default App;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: 'red',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   }
+// });
